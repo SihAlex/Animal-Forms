9c283adda02d13151613a763f502802e937dc5bf
SihAlex <siheev.alex@gmail.com>
2021-08-03 16:19:02 +0300

    (Dev) Fixed conflicts on branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import Header from "./components/Header";
-<<<<<<< HEAD
-import Main from "./scenes/main/Main";
-import Footer from "./components/Footer";
-import { CovidStatsContextProvider } from "./components/store/covid-stasts-context";
-import { createTheme, ThemeProvider } from "@material-ui/core";
-
-=======
 import Main from "./pages/main/Main";
 import Info from "./pages/info/Info";
 import { CovidStatsContextProvider } from "./components/store/covid-stasts-context";
@@ -13,7 +6,6 @@ import { createTheme, ThemeProvider } from "@material-ui/core";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 
->>>>>>> Added routing.
 const theme = createTheme({
   palette: {
     primary: {
@@ -33,10 +25,6 @@ function App() {
     <CovidStatsContextProvider>
       <ThemeProvider theme={theme}>
         <Header />
-<<<<<<< HEAD
-        <Main />
-        <Footer />
-=======
         <Switch>
           <Route path="/" exact>
             <Redirect to="/main" />
@@ -51,7 +39,6 @@ function App() {
             <h1>Page not found</h1>
           </Route>
         </Switch>
->>>>>>> Added routing.
       </ThemeProvider>
     </CovidStatsContextProvider>
   );
